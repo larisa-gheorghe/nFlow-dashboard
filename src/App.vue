7700711335b409af7e47d2909446613fb22b1f83
Larisa Zaharia <larisa.camelia.gheorghe@gmail.com>
2023-01-02 16:44:59 +0200

    Create Header Component

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <app-header />
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,35 @@
+<template>
+  <nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">nFlow</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-link px-3" href="#">Workflow Instances</a>
+          <a class="nav-link px-3" href="#">Workflow Definitions</a>
+          <a class="nav-link px-3" href="#">Executors</a>
+          <a class="nav-link px-3" href="#">About</a>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "app-header",
+};
+</script>
+
+<style>
+</style>
