34ce7eab675938a814acccf0d99936c911c280ab
Larisa Zaharia <larisa.camelia.gheorghe@gmail.com>
2023-01-11 20:03:14 +0200

    change table box-shadow

--- src/components/BulkDetails.vue
+++ src/components/BulkDetails.vue
@@ -201,7 +201,7 @@ export default {
 </script>
 
 <style>
-    .table-responsive {
+    #settingsTable, #statsTable {
         box-shadow: 0px 2px 1px -1px rgb(0 0 0 / 20%), 0px 1px 1px 0px rgb(0 0 0 / 14%), 0px 1px 3px 0px rgb(0 0 0 / 12%);
     }
 </style>
